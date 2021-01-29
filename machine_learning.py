523b65ed18998b39c4c1b72fbd931552a5770786
fernando <fernando.h.vinha@gmail.com>
2021-01-29 14:28:11 -0300

    xD

--- machine_learning.py
+++ machine_learning.py
@@ -13,7 +13,7 @@ x = data.drop('cardio', axis = 1)
 #criando os conjuntos de dados de treino e teste: 
 x_treino, x_teste, y_treino, y_teste = train_test_split(x, y, test_size = 0.3)
 #Criação do modelo: 
-modelo = ExtraTreesClassifier(n_estimatore=100) 
+modelo = ExtraTreesClassifier() 
 modelo.fit(x_treino, y_treino)
 #Imprimindo resultados: 
 precisao = modelo.score(x_teste, y_teste) 
