cdfaf8d86c4c56e629fb2ed2c1c35c690de7e89d
fernando <fernando.h.vinha@gmail.com>
2021-01-29 10:33:43 -0300

    cardio
    cardio

--- machine_learning.py
+++ machine_learning.py
@@ -4,10 +4,10 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import ExtraTreesClassifier
 
-data = pd.read_csv('/home/ec2-user/Heart-Attack-30/cardio_train.csv')
+data = pd.read_csv('cardio_train.csv')
 
 #Separando as variáveis entre preditoras e variável alvo 
-#y = data['cardio'] 
+y = data['cardio'] 
 x = data.drop('cardio', axis = 1)
 
 #criando os conjuntos de dados de treino e teste: 
