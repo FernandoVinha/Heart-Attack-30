4016e8860b9510247ef6506a0a60447355b6aaed
fernando <fernando.h.vinha@gmail.com>
2021-01-29 10:25:07 -0300

    ml
    teste6

--- machine_learning.py
+++ machine_learning.py
@@ -4,11 +4,11 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import ExtraTreesClassifier
 
-data = pd.read_csv("/home/ec2-user/Heart-Attack-30/cardio_train.csv")
+data = pd.read_csv('/home/ec2-user/Heart-Attack-30/cardio_train.csv')
 
 #Separando as variáveis entre preditoras e variável alvo 
-y = data["cardio"] 
-x = data.drop("cardio", axis = 1)
+y = data['cardio'] 
+x = data.drop('cardio', axis = 1)
 
 #criando os conjuntos de dados de treino e teste: 
 x_treino, x_teste, y_treino, y_teste = train_test_split(x, y, test_size = 0.3)
