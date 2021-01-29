dbd7d31bb092ef905befe72cb68760b0868b2570
fernando <fernando.h.vinha@gmail.com>
2021-01-29 10:36:14 -0300

    voltar a estudo de aws
    aws

--- machine_learning.py
+++ machine_learning.py
@@ -4,17 +4,17 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import ExtraTreesClassifier
 
-data = pd.read_csv('cardio_train.csv')
+data = pd.read_csv('/home/ec2-user/Heart-Attack-30/cardio_train.csv')
 
 #Separando as variáveis entre preditoras e variável alvo 
 y = data['cardio'] 
 x = data.drop('cardio', axis = 1)
 
 #criando os conjuntos de dados de treino e teste: 
-#x_treino, x_teste, y_treino, y_teste = train_test_split(x, y, test_size = 0.3)
+x_treino, x_teste, y_treino, y_teste = train_test_split(x, y, test_size = 0.3)
 #Criação do modelo: 
-#modelo = ExtraTreesClassifier(n_estimatore=100) 
-#modelo.fit(x_treino, y_treino)
+modelo = ExtraTreesClassifier(n_estimatore=100) 
+modelo.fit(x_treino, y_treino)
 #Imprimindo resultados: 
-#resultado = modelo.score(x_teste, y_teste) 
-#print("Precisão:", resultado)
+resultado = modelo.score(x_teste, y_teste) 
+print("Precisão:", resultado)
