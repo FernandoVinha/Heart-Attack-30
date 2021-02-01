f5851a390aa8ec480450ce4392d0e8b175982383
fernando <fernando.h.vinha@gmail.com>
2021-02-01 15:28:54 -0300

    ip externo
    testa o ip externo

--- data.py
+++ data.py
@@ -1,5 +1,10 @@
 import machine_learning
+import csv
 
 def insert(age,gender,height,weight,ap_hi,ap_lo,cholesterol,gluc,smoke,alco,active):
     
+   with open('dados_api.csv', 'w', newline='') as file:
+       writer = csv.writer(file)
+       writer.writerow(["age","gender","height","weight","ap_hi,ap_lo","cholesterol","gluc,smoke","alco","active","cardio"])
+
     return ("age" )
--- nova_tabela.py
+++ nova_tabela.py
@@ -1,5 +1,8 @@
 import csv
 
- with open('dados_api.csv', 'w', newline='') as file:
-       writer = csv.writer(file)
-       writer.writerow(["age","gender","height","weight","ap_hi,ap_lo","cholesterol","gluc,smoke","alco","active","cardio"])
+with open('dados_api.csv', 'w', newline='') as csvfile:
+    fieldnames = ['age', 'gender', 'height', 'weight', 'ap_hi,ap_lo', 'cholesterol', 'gluc','gluc', 'smoke', 'alco', 'active', 'cardio']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+    writer.writeheader()
+    writer.writerow(['18393','2','168','62.0','110,80','1','1','0','0','1,0'])
